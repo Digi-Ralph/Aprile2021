e8a94e06a90897301600fa5d035b3b9d047f72a3
Digi-Ralph <74498402+Digi-Ralph@users.noreply.github.com>
2021-05-02 21:00:28 +0000

    Add files via upload

---
+++ Concept19/index.js
@@ -0,0 +1,26 @@
+let imgArr = document.querySelectorAll('.img');
+let prevX = 0;
+let prevY = 0;
+let moveXAmount = 0;
+let moveYAmount = 0;
+
+document.addEventListener('mousemove', function (e) {
+	mousePos(e);
+});
+
+function mousePos(e) {
+	moveXAmount = e.pageX - prevX;
+	moveYAmount = e.pageY - prevY;
+	moveImg(moveXAmount, moveYAmount);
+	prevX = e.pageX;
+	prevY = e.pageY;
+}
+
+function moveImg(xAmount, yAmount) {
+	imgArr.forEach(function (img) {
+		let movementStrenght = 5 + Math.random() * 15;
+
+		img.style.left = img.offsetLeft - xAmount / movementStrenght + 'px';
+		img.style.top = img.offsetTop - yAmount / movementStrenght + 'px';
+	});
+}
